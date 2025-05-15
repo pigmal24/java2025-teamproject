ecf91259b52b2beca7b382a6650ccb627ed1079e
Kjh-0629 <89411104+Kjh-0629@users.noreply.github.com>
2025-05-15 20:12:59 +0900

    MOHANO basic file

---
+++ MOHANO/src/handle/Task.java
@@ -0,0 +1,11 @@
+package handle;
+
+public class Task {
+	private String taskSubject;
+	private String taskName;
+	private String deadLine;
+	public Task(String sub, String name, String deadLine) {
+		this.taskSubject = sub;
+		this.taskName = name;
+	}
+}
