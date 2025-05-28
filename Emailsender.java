4d01b797522eb65625a7f8a663890434d4546c8c
ddogas01 <thinkdust0920@gmail.com>
2025-05-28 18:51:23 +0900

    Update EmailSender.java

--- Emailsender.java
+++ Emailsender.java
@@ -2,37 +2,45 @@ package handle2;
 
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Properties;
-import java.util.Scanner;
-import java.util.concurrent.*;
 
-
-import java.util.Properties;
 
 public class EmailSender {
 
     // user, task 정보 받아 이메일 전송
-    public void sendUserTaskEmail(User user, Task task) {
+    public void sendUserTaskEmail(User user, List<Task> todayTask) {
         String senderEmail = user.getEmailAddress();
         String senderPassword = user.getSmtpPass();
         String receiverEmail = senderEmail;  // 자기 자신에게 이메일 발송
 
-        String subject = "[과제 안내] " + task.getTitle();
-        String body = "다음 과제가 등록되었습니다.\n"
-                    + "과목: " + task.getSubject() + "\n"
-                    + "과제: " + task.getTitle() + "\n"
-                    + "마감일: " + task.getDeadline().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\n\n";
-
+        // 처음 로그인 / 과제 비었을때 -> 환영 이메일 (과제입력해주세요) (todaytask 가 null)
+ 
+        String subject; 
+        String body = "";
+        
+        if (todayTask == null || todayTask.isEmpty()) // list 
+        {
+        	subject = "환영합니다";
+        	body += "과제를 입력해주세요!";
+        	
+        }
+        
+        else {
+        	subject = "과제 알림입니다.";
+        	for(Task a : todayTask) {
+        		body += 
+        				"과목: " + a.getSubject() + "\n"
+        						+ "과제: " + a.getTitle() + "\n"
+        						+ "마감일: " + a.getDeadline().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\n\n";
+        		} // todaytask 과제가 출력됨
+            body += "기한 내에 꼭 제출해 주세요!\n";
+        	}
+        
         sendEmail(senderEmail, senderPassword, receiverEmail, subject, body);
     }
-    
-    public static void main(String[] args) {
-	
-    	new EmailSender().sendEmail(senderEmail, senderPassword, receiverEmail, subject, body);
-
-	}
+ 
 
 	// 실제 이메일 전송 메서드
     public void sendEmail(String senderEmailAddress, String senderEmailPassword,
